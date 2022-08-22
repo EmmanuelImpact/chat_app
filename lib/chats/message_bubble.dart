d194a83810ad2e05247d9ed2a8b86ec4a581f05b
John Doe <ayobamioyediran2018@yahoo.com>
2022-08-22 09:16:37 +0000

    chat commit

--- lib/chats/message_bubble.dart
+++ lib/chats/message_bubble.dart
@@ -16,51 +16,62 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            // ignore: deprecated_member_use
-            color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
-            borderRadius: BorderRadius.only(
-              topLeft: const Radius.circular(12),
-              topRight: const Radius.circular(12),
-              bottomLeft:
-                  isMe ? const Radius.circular(12) : const Radius.circular(0),
-              bottomRight:
-                  isMe ? const Radius.circular(0) : const Radius.circular(12),
-            ),
-          ),
-          padding: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          margin: const EdgeInsets.symmetric(
-            vertical: 4,
-            horizontal: 8,
-          ),
-          width: 200,
-          child: Column(
-            crossAxisAlignment:
-                isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            children: [
-              Text(
-                username,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                message,
-                style: TextStyle(
-                  color: isMe
-                      ? Colors.black
-                      // ignore: deprecated_member_use
-                      : Theme.of(context).accentTextTheme.headline1!.color,
+        CircleAvatar(
+          radius: 20,
+          backgroundImage: NetworkImage(userImage),
+        ),
+        Row(
+          mainAxisAlignment:
+              isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                // ignore: deprecated_member_use
+                color: isMe ? Colors.grey[300] : Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(12),
+                  topRight: const Radius.circular(12),
+                  bottomLeft: isMe
+                      ? const Radius.circular(12)
+                      : const Radius.circular(0),
+                  bottomRight: isMe
+                      ? const Radius.circular(0)
+                      : const Radius.circular(12),
                 ),
-                textAlign: isMe ? TextAlign.end : TextAlign.start,
               ),
-            ],
-          ),
+              padding: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              margin: const EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 8,
+              ),
+              width: 200,
+              child: Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    username,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    message,
+                    style: TextStyle(
+                      color: isMe
+                          ? Colors.black
+                          // ignore: deprecated_member_use
+                          : Theme.of(context).accentTextTheme.headline1!.color,
+                    ),
+                    textAlign: isMe ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ],
     );
